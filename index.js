ffee3ca98bc588073bd611ae1fe9229d7a03a83e
Andy-Fernandez <58851171+Andy-Fernandez@users.noreply.github.com>
2023-02-15 23:43:38 -0400

    Alto de las secciónes modificadas, y se añadió un borde inferior al navbar

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const navToggle = document.querySelector(".nav-toggle");
 const navMenu = document.querySelector(".navbar-nav");
+const navLink = document.querySelectorAll(".nav-link");
 
 navToggle.addEventListener("click", () => {
   navMenu.classList.toggle("navbar-nav_visible");
@@ -10,3 +11,14 @@ navToggle.addEventListener("click", () => {
     navToggle.setAttribute("aria-label", "Abrir menú");
   }
 });
+
+function closeMenu() {
+  navMenu.classList.remove("navbar-nav_visible");
+  navToggle.setAttribute("aria-label", "Abrir menú");
+}
+
+console.log(navLink);
+if (navLink) {
+  navLink.forEach((n) => n.addEventListener("click", closeMenu));
+}
+
